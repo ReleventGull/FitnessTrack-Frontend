79c35b890a374de39a216974346bcb29115e3329
kaypaint <kelanmpainter@gmail.com>
2022-12-27 19:34:54 -0600

    feat(api): Added loginUser fetch function

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,26 @@
-const BASEURL = "https://fitnesstrac-kr.herokuapp.com/api/2207-FTB-ET-WEB-PT";
+const BASEURL = "https://fitnesstrac-kr.herokuapp.com/api";
 
 const registerUser = async({username, password}) => {
 
 }
 
 const loginUser = async({username, password}) => {
-
+    try {
+        const response = await fetch(`${BASEURL}/users/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: username, 
+                password: password
+            })
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("An error occured while attempting to login", error);
+    }
 }
 
 const getAllActivities = async() => {
