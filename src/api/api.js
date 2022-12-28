7826697b1df72b9a522bc922480c5ad2295134bc
MShiers7 <martyshiers7@gmail.com>
2022-12-27 19:32:28 -0600

    feat: registerUser

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,25 @@
-const BASEURL = "https://fitnesstrac-kr.herokuapp.com/api/2207-FTB-ET-WEB-PT";
+const BASEURL = "https://fitnesstrac-kr.herokuapp.com/api";
 
 const registerUser = async({username, password}) => {
-
+    try {
+        const response = await fetch(`${BASEURL}/users/register`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    });
+    const data = await response.json();
+    return data;
+} catch (error) {
+    console.error("An error occured while attempting to register", error);
 }
+};
+
+
 
 const loginUser = async({username, password}) => {
 
