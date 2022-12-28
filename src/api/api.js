f600e9a88dd3b4b7ee593ea0f4b3db778d23ea10
Jaron <jaronchretien@yahoo.com>
2022-12-27 20:20:42 -0600

    feat(routes): Routes updated

--- src/api/api.js
+++ src/api/api.js
@@ -50,3 +50,5 @@ export const getAllActivities = async() => {
     }
 }
 
+
+
--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export {default as Routines} from './Routines';
 export {default as Register} from './Register'
 export {default as Header} from './Header'
 export {default as Login} from './Login'
+export {default as Activities} from './Activities'
