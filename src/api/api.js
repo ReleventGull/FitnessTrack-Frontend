cc60dbc104fe3c37370579b77b0508ca3c6b0c49
MShiers7 <martyshiers7@gmail.com>
2022-12-31 19:24:17 -0600

    feat: attachActivityToRoutine

--- src/api/api.js
+++ src/api/api.js
@@ -154,6 +154,29 @@ export const createRoutine = async({token, name, goal}) => {
     }
 }
 
+export const attachActivityToRoutine = async({activityId, count, duration, token}) => {
+    try{
+        const response = await fetch(`https://fitnesstrac-kr.herokuapp.com/api/routines/${id}/activities`, {
+            method: "POST",
+            headers: {
+                'Content-type': 'Application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                activityId: activityId,
+                count: count,
+                duration: duration
+            })
+        })
+        const result = await response.json()
+        return result
+    }catch(error) {
+        console.error(error)
+    }
+}
+
+
+
 export const deleteUserRoutine = async(token) => {
     try {
         const response = await fetch(`https://fitnesstrac-kr.herokuapp.com/api/routines/${id}`, {
